26af2cc9980c600a50409898a5a2198099d75d49
pythonbyfebash <pythonbyfebash@gmail.com>
2023-11-06 14:59:52 +0530

    add Edit functionality

--- todoGUI.py
+++ todoGUI.py
@@ -4,9 +4,13 @@ import todo_functions as tf
 todo_label = sg.Text('Type in a todo:')
 todo_input= sg.InputText(tooltip='Enter todo',key='todo_input')
 add_button = sg.Button('Add')
+todo_listbox = sg.Listbox(values=tf.get_todos(),key="todos_listbox",
+                          size=[45,10],enable_events=True)
+edit_button = sg.Button('Edit')
 
 window = sg.Window(title="My ToDos",
-                   layout=[[todo_label],[todo_input,add_button]],
+                   layout=[[todo_label],[todo_input,add_button],
+                           [todo_listbox,edit_button]],
                    font= ('Courier 12',20))
 
 
@@ -20,8 +24,23 @@ while True:
             new_todo = values['todo_input'] + "\n"
             todos_list.append(new_todo)
             tf.write_todos(todos_list)
+            window['todos_listbox'].update(values=todos_list)
         case sg.WIN_CLOSED:
             break
+        case 'Edit':
+            selected_text = values['todos_listbox'][0]
+            edit_value = values['todo_input']
+            todos = tf.get_todos()
+            index = todos.index(selected_text)
+            todos[index] = edit_value + "\n"
+            tf.write_todos(todos)
+            window['todos_listbox'].update(values=todos)
+        case 'todos_listbox':
+            selected_text = values['todos_listbox'][0]
+            window['todo_input'].update(value=selected_text)
+
+
+
 
 window.close()
 
