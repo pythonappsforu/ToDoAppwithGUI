7dd9136d74e566b1bc912aeb115537a690aca571
pythonbyfebash <pythonbyfebash@gmail.com>
2023-11-07 11:00:55 +0530

    Add clock,pysimpleGUI theme,error fix for edit complete button

--- todoGUI.py
+++ todoGUI.py
@@ -1,9 +1,13 @@
 import PySimpleGUI as sg
 import todo_functions as tf
+import time
 
+sg.theme('GreenMono')
+
+clock = sg.Text(key='clock')
 todo_label = sg.Text('Type in a todo:')
 todo_input= sg.InputText(tooltip='Enter todo',key='todo_input')
-add_button = sg.Button('Add')
+add_button = sg.Button('Add',size=10)
 todo_listbox = sg.Listbox(values=tf.get_todos(),key="todos_listbox",
                           size=[45,10],enable_events=True)
 edit_button = sg.Button('Edit')
@@ -12,15 +16,16 @@ exit_button = sg.Button('Exit')
 
 
 window = sg.Window(title="My ToDos",
-                   layout=[[todo_label],[todo_input,add_button],
+                   layout=[[clock],[todo_label],[todo_input,add_button],
                            [todo_listbox,edit_button,complete_button],[exit_button]],
                    font= ('Courier 12',20))
 
 
 while True:
-    event,values = window.read()
+    event,values = window.read(timeout=800)
     print(event)
     print(values)
+    window['clock'].update(time.strftime('%b %d,%Y %H:%M:%S'))
     match event:
         case 'Add':
             todos_list = tf.get_todos()
@@ -31,20 +36,27 @@ while True:
         case sg.WIN_CLOSED:
             break
         case 'Edit':
-            selected_text = values['todos_listbox'][0]
-            edit_value = values['todo_input']
-            todos = tf.get_todos()
-            index = todos.index(selected_text)
-            todos[index] = edit_value + "\n"
-            tf.write_todos(todos)
-            window['todos_listbox'].update(values=todos)
+            try:
+                selected_text = values['todos_listbox'][0]
+                edit_value = values['todo_input']
+                todos = tf.get_todos()
+                index = todos.index(selected_text)
+                todos[index] = edit_value + "\n"
+                tf.write_todos(todos)
+                window['todos_listbox'].update(values=todos)
+            except IndexError:
+                sg.Popup('Please select a todo to edit',font=('Courier 12',20))
+
         case 'Complete':
-            selected_text = values['todos_listbox'][0]
-            todos = tf.get_todos()
-            todos.remove(selected_text)
-            tf.write_todos(todos)
-            window['todos_listbox'].update(values=todos)
-            window['todo_input'].update(value='')
+            try:
+                selected_text = values['todos_listbox'][0]
+                todos = tf.get_todos()
+                todos.remove(selected_text)
+                tf.write_todos(todos)
+                window['todos_listbox'].update(values=todos)
+                window['todo_input'].update(value='')
+            except IndexError:
+                sg.Popup('Please select a todo to mark complete',font=('Courier 12',20))
 
 
         case 'todos_listbox':
